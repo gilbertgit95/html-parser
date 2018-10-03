0d8b1d48dbf6798e8a53eccb47e6ae69da45238e
Kagiwara <gilbert.cuerbo@gmail.com>
2018-10-03 21:56:14 +0800

    add regex for cleaning html strings

--- html-parser.js
+++ html-parser.js
@@ -22,16 +22,35 @@
 })(() => {
   'use strict'
   // the parser factory
+  // regex character detector
+  const LINE_BREAK_REGEX = /\r?\n|\r/g
+  const MULTIPLE_SPACE_REGEX = /( {2})+/g
+  const HTML_COMMENT_REGEX = /(<!--)+(.*)+(-->)/g
 
   // the local function global to the main class
   // reusable functions
-  let toJsonParser = (htmlString) => {
+
+  // clean all unwanted codes or characters
+  let cleanHtmlString = htmlString => {
+    // remove all html comments
+    // multiple spaces and line Breaks
+    return htmlString
+              .replace(HTML_COMMENT_REGEX, '')
+              .replace(MULTIPLE_SPACE_REGEX, '')
+              .replace(LINE_BREAK_REGEX, '')
+  }
+
+  let toJsonParser = htmlString => {
     let parsed = {}
 
     // check if the parameter is really a string
     // then start the process of parsing
     if (typeof htmlString == 'string') {
-      
+      // remove unwanted codes
+      let cleanHtml = cleanHtmlString(htmlString)
+
+      console.log('original:', htmlString)
+      console.log('cleaned:', cleanHtml)
     }
 
     return parsed
